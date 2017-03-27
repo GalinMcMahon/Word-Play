71e157a2809aeec0b5366c62ef53d035589270c2
Galin McMahon and Tyler Stephenson <tyler@>
2017-03-27 16:05:13 -0700

    create site that uses js to manipulate a sentaence

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(function(){
 
+  var longWords = [];
 
-  
+  $("#submit").click(function(event) {
+    var originalSentence = $("#sentence").val();
+    var space = " ";
+    var splitSentence = originalSentence.split(space);
+    var three = splitSentence.forEach(function(split){
+      if (split.length > 2){
+        longWords.push(split);
+      }
+    });
+    var reversedThree = longWords.reverse().toString().toUpperCase();
+    var reversedLongWords = reversedThree.replace(/[,.]/g , " ");
+    $("#result").text(reversedLongWords);
+    event.preventDefault();
+  });
 });
